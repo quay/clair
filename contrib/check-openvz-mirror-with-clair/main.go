8b137e8a95a1755aacd3e97b707c647f96a9c3ac
Sergei A Mamonov <mrqwer88@gmail.com>
2015-11-30 00:13:19 +0300

    contrib: add copyright in check-openvz-mirror-with-clair

--- contrib/check-openvz-mirror-with-clair/main.go
+++ contrib/check-openvz-mirror-with-clair/main.go
@@ -1,3 +1,17 @@
+// Copyright 2015 FastVPS Eesti OU
+//
+// Licensed under the GNU GENERAL PUBLIC LICENSE, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
