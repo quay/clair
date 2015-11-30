75aff0382a567a29ef12d5003e8f8d7cbba092bf
Sergei A Mamonov <mrqwer88@gmail.com>
2015-11-30 23:11:18 +0300

    contrib: check-openvz-mirror-with-clair fix license

--- contrib/check-openvz-mirror-with-clair/main.go
+++ contrib/check-openvz-mirror-with-clair/main.go
@@ -1,10 +1,10 @@
 // Copyright 2015 FastVPS Eesti OU
 //
-// Licensed under the GNU GENERAL PUBLIC LICENSE, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
