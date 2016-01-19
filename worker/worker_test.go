82175dcfe9e36766c5e88199d8045e2f0733f483
Quentin Machu <me@quentin-machu.fr>
2016-01-19 15:16:45 -0500

    *: add missing copyright headers

--- worker/worker_test.go
+++ worker/worker_test.go
@@ -1,3 +1,17 @@
+// Copyright 2015 clair authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package worker
 
 import (
@@ -5,14 +19,12 @@ import (
 	"runtime"
 	"testing"
 
-	// Register the required detectors.
-
 	"github.com/coreos/clair/database"
 	"github.com/coreos/clair/database/pgsql"
 	"github.com/coreos/clair/utils/types"
 	"github.com/stretchr/testify/assert"
-
-	// Register detectors
+  
+	// Register the required detectors.
 	_ "github.com/coreos/clair/worker/detectors/data"
 	_ "github.com/coreos/clair/worker/detectors/feature/dpkg"
 	_ "github.com/coreos/clair/worker/detectors/namespace/aptsources"
