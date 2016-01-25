712aa11b8b566be8a1c471481667c6a6f633f08f
Quentin Machu <me@quentin-machu.fr>
2016-01-25 12:58:51 -0500

    updater: Add support for Ubuntu Vivid Core and ignore Vivid PhoneOverlay
    Reacts to https://bazaar.launchpad.net/~ubuntu-security/ubuntu-cve-tracker/master/revision/10488

--- database/os_mapping.go
+++ database/os_mapping.go
@@ -34,11 +34,12 @@ var DebianReleasesMapping = map[string]string{
 // UbuntuReleasesMapping translates Ubuntu code names to version numbers
 // TODO That should probably be stored in the database or in a file
 var UbuntuReleasesMapping = map[string]string{
-	"precise": "12.04",
-	"quantal": "12.10",
-	"raring":  "13.04",
-	"trusty":  "14.04",
-	"utopic":  "14.10",
-	"vivid":   "15.04",
-	"wily":    "15.10",
+	"precise":           "12.04",
+	"quantal":           "12.10",
+	"raring":            "13.04",
+	"trusty":            "14.04",
+	"utopic":            "14.10",
+	"vivid":             "15.04",
+	"vivid/ubuntu-core": "15.04-core",
+	"wily":              "15.10",
 }
