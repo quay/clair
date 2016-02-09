e8b16175effcff9b9ead13aeadab7897f5331d37
Quentin Machu <me@quentin-machu.fr>
2016-02-09 18:12:44 -0500

    api: return 400 if we can't extract a layer

--- utils/http/http.go
+++ utils/http/http.go
@@ -21,6 +21,7 @@ import (
 	"net/http"
 
 	"github.com/coreos/clair/database"
+	"github.com/coreos/clair/utils"
 	cerrors "github.com/coreos/clair/utils/errors"
 	"github.com/coreos/clair/worker"
 )
@@ -55,7 +56,7 @@ func WriteHTTPError(w http.ResponseWriter, httpStatus int, err error) {
 				httpStatus = http.StatusNotFound
 			case database.ErrTransaction, database.ErrBackendException:
 				httpStatus = http.StatusServiceUnavailable
-			case worker.ErrParentUnknown, worker.ErrUnsupported:
+			case worker.ErrParentUnknown, worker.ErrUnsupported, utils.ErrCouldNotExtract, utils.ErrExtractedFileTooBig:
 				httpStatus = http.StatusBadRequest
 			}
 		}
