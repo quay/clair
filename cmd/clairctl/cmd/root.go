21e097e8f08c800b29411165aa624d97398b2d8c
jgsqware <garciagonzalez.julien@gmail.com>
2016-07-01 10:34:42 +0200

    Fix wrong license and bad comment

--- cmd/clairctl/cmd/root.go
+++ cmd/clairctl/cmd/root.go
@@ -1,17 +1,3 @@
-// Copyright © 2016 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
@@ -34,10 +20,6 @@ var RootCmd = &cobra.Command{
 	Use:   "clairctl",
 	Short: "Analyze your docker image with Clair, directly from your registry or local images.",
 	Long:  ``,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) {
-	// },
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
