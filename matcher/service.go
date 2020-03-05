445c6a82000bc2b8b5daf341d0cdbb57e6d7e641
ldelossa <ldelossa@redhat.com>
2020-03-05 00:03:33 -0500

    refactor services
    Signed-off-by: ldelossa <ldelossa@redhat.com>

--- matcher/service.go
+++ matcher/service.go
@@ -6,8 +6,15 @@ import (
 	"github.com/quay/claircore"
 )
 
-// Service creates an interface around claircore.Libvuln
+// Service is an aggregate interface wrapping claircore.Libvuln functionality.
+//
+// Implementation may use a local instance of claircore.Libindex or a remote
+// instance via http or grpc client.
 type Service interface {
-	// Scan(ctx context.Context, manifestHash string) (*claircore.VulnerabilityReport, error)
+	Scanner
+}
+
+// Scanner is an interface providing a claircore.VulnerabilityReport given a claircore.IndexReport
+type Scanner interface {
 	Scan(ctx context.Context, ir *claircore.IndexReport) (*claircore.VulnerabilityReport, error)
 }
