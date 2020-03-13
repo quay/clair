4373f9c12b25600a84b3ddea3374cb1352b3939c
Hank Donnay <hdonnay@redhat.com>
2020-03-13 17:04:50 -0400

    clairctl: start on clair cli tool
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

---
+++ cmd/clairctl/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+var (
+	flagDebug bool
+)
+
+func main() {
+	var exit int
+	defer func() { os.Exit(exit) }()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	app := &cli.App{
+		Name:                 "clairctl",
+		Version:              "0.1.0",
+		Description:          "A command-line tool for clair v4.",
+		EnableBashCompletion: true,
+		Before: func(c *cli.Context) error {
+			if c.IsSet("D") {
+				debug.SetOutput(os.Stderr)
+			}
+			return nil
+		},
+		Commands: []*cli.Command{
+			ManifestCmd,
+		},
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:  "D",
+				Usage: "print debugging logs",
+			},
+		},
+	}
+	log.SetFlags(log.Flags())
+
+	if err := app.RunContext(ctx, os.Args); err != nil {
+		exit = 1
+		if err, ok := err.(cli.ExitCoder); ok {
+			exit = err.ExitCode()
+		}
+		log.Println(err)
+	}
+}
