10b87578aa55ecaed27d36964b1de18e7eaffe42
mcoops <cooper.d.mark@gmail.com>
2020-09-16 00:28:04 +1000

    logging: add similar logging to v3
    Add back in HTTP logging to what was present in v3, to each of the handlers.
    
    Signed-off-by: mcoops <cooper.d.mark@gmail.com>

---
+++ httptransport/logginghandler.go
@@ -0,0 +1,34 @@
+package httptransport
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/rs/zerolog"
+)
+
+type httpStatusWriter struct {
+	http.ResponseWriter
+
+	StatusCode int
+}
+
+// LoggingHandler will log HTTP requests using the pre initialized zerolog.
+func LoggingHandler(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		log := zerolog.Ctx(r.Context())
+
+		next.ServeHTTP(w, r)
+
+		log.Info().
+			Str("remote addr", r.RemoteAddr).
+			Str("method", r.Method).
+			Str("request uri", r.RequestURI).
+			Str("status", strconv.Itoa(http.StatusOK)).
+			Float64("elapsed time (md)", float64(time.Since(start).Nanoseconds())*1e-6).
+			Msg("handled HTTP request")
+	}
+}
