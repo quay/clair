164219e8a9ea2d8d819509a185ace12a5ceb46f2
Hank Donnay <hdonnay@redhat.com>
2022-02-03 12:53:45 -0600

    postgres: add Init function
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- notifier/postgres/store.go
+++ notifier/postgres/store.go
@@ -2,20 +2,48 @@ package postgres
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 
 	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/jackc/pgx/v4/stdlib"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/quay/zlog"
+	"github.com/remind101/migrate"
+
+	"github.com/quay/clair/v4/notifier/migrations"
 )
 
-// Store implements the notifier.Store interface
+// Store implements the notifier.Store interface.
 type Store struct {
 	pool *pgxpool.Pool
 }
 
+// NewStore returns a Store using the passed-in Pool.
+//
+// The caller should close the Pool once the store is no longer needed.
 func NewStore(pool *pgxpool.Pool) *Store {
 	return &Store{pool}
+}
+
+// Init initializes the database using the specified config.
+func Init(ctx context.Context, cfg *pgx.ConnConfig) error {
+	ctx = zlog.ContextWithValues(ctx, "component", "notifier/postgres/Init")
+	db, err := sql.Open("pgx", stdlib.RegisterConnConfig(cfg))
+	if err != nil {
+		return fmt.Errorf("failed to open db: %w", err)
+	}
+	defer db.Close()
+
+	zlog.Info(ctx).Msg("performing notifier migrations")
+	migrator := migrate.NewPostgresMigrator(db)
+	migrator.Table = migrations.MigrationTable
+	if err := migrator.Exec(migrate.Up, migrations.Migrations...); err != nil {
+		return fmt.Errorf("failed to perform migrations: %w", err)
+	}
+	return nil
 }
 
 // As a rule of thumb, admin/worker locks should be in the two-part keyspace to
