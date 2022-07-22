b8882f9ddb1d3801fdbb47705a68640cfd819aab
Hank Donnay <hdonnay@redhat.com>
2022-07-22 13:34:05 -0500

    clair: better argument error messages
    This replaces the custom flag values with just visiting the flags and
    processing the string argument at that time.
    
    Closes: #1605
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- cmd/clair/main.go
+++ cmd/clair/main.go
@@ -35,19 +35,44 @@ const (
 
 func main() {
 	// parse conf from cli
-	var (
-		confFile ConfValue
-		conf     config.Config
-		runMode  ConfMode
-	)
-	confFile.Set(os.Getenv(envConfig))
-	runMode.Set(os.Getenv(envMode))
-	flag.Var(&confFile, "conf", "The file system path to Clair's config file.")
-	flag.Var(&runMode, "mode", "The operation mode for this server.")
+	var conf config.Config
+	flag.String("conf", "", "The file system path to Clair's config file.")
+	flag.String("mode", "combo", "The operation mode for this server.")
 	flag.Parse()
-	if confFile.String() == "" {
-		golog.Fatalf("must provide a -conf flag or set %q in the environment", envConfig)
-	}
+	flag.VisitAll(func(f *flag.Flag) {
+		fv := f.Value.(flag.Getter).Get().(string)
+		var key string
+		switch f.Name {
+		case "conf":
+			key = envConfig
+		case "mode":
+			key = envMode
+		}
+		v, ok := os.LookupEnv(key)
+		if fv == "" && !ok {
+			golog.Fatalf("must provide a -%s value or set %q in the environment", f.Name, key)
+		}
+		if fv == "" && ok {
+			fv = v
+		}
+		switch f.Name {
+		case "conf":
+			cf, err := os.Open(fv)
+			if err != nil {
+				golog.Fatalf("failed to open config file: %v", err)
+			}
+			defer cf.Close()
+			if err := yaml.NewDecoder(cf).Decode(&conf); err != nil {
+				golog.Fatalf("failed to decode yaml config: %v", err)
+			}
+		case "mode":
+			m, err := config.ParseMode(fv)
+			if err != nil {
+				golog.Fatalf("bad mode %q: %v", fv, err)
+			}
+			conf.Mode = m
+		}
+	})
 	fail := false
 	defer func() {
 		if fail {
@@ -55,12 +80,6 @@ func main() {
 		}
 	}()
 
-	// validate config
-	err := yaml.NewDecoder(confFile.file).Decode(&conf)
-	if err != nil {
-		golog.Fatalf("failed to decode yaml config: %v", err)
-	}
-	conf.Mode = runMode.Mode
 	// Grab the warnings to print after the logger is configured.
 	ws, err := config.Validate(&conf)
 	if err != nil {
