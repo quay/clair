3c37b8fce34c18011007409ff767af52458db21c
Will Garcia <willgrod@amazon.com>
2023-04-16 16:43:27 +1000

    feat: create repo using puppeteer

--- puppeteer/app.js
+++ puppeteer/app.js
@@ -17,18 +17,24 @@ const clickByText = async (page, text) => {
   };
 
 (async () => {
-	const browser = await puppeteer.launch({headless: false});
+	const browser = await puppeteer.launch({headless: false,  args: ['--no-sandbox', '--disable-setuid-sandbox'],});
 	const page = await browser.newPage();
 	await page.goto(process.env.QUAY_URL);
 
   await clickByText(page, `Create Account`);
   await page.waitForSelector('input[placeholder="Requested username"]');
   await page.type('input[placeholder="Requested username"]', process.env.QUAY_USERNAME, {delay: 100});
-  await page.type('input[placeholder="Your email address"]', process.env.QUAY_EMAIL);
-  await page.type('input[placeholder="Create a password"]', process.env.QUAY_PASSWORD);
-  await page.type('input[placeholder="Verify your password"]', process.env.QUAY_PASSWORD); 
+  await page.type('input[placeholder="Your email address"]', process.env.QUAY_EMAIL, {delay: 100});
+  await page.type('input[placeholder="Create a password"]', process.env.QUAY_PASSWORD, {delay: 100});
+  await page.type('input[placeholder="Verify your password"]', process.env.QUAY_PASSWORD, {delay: 100}); 
   
   await page.click('button[id="signupButton"]');
 
-	await browser.close();
+  await page.waitForSelector('ul[class="namespaces-list"]');
+  await page.click('a[href="/new/"]');
+
+  await page.waitForSelector('input[id="repoName"]');
+  await page.type('input[id="repoName"]', 'gold-fish', {delay: 100});
+
+  await page.keyboard.press('Enter');
 })();
