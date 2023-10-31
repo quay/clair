718ef948653b85198caaafb50db1ca64fe5706e4
Hank Donnay <hdonnay@redhat.com>
2023-10-31 12:10:57 -0500

    cicd: make nightly script shellcheck-clean
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- .github/script/nightly-module.sh
+++ .github/script/nightly-module.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
-: ${CLAIRCORE_BRANCH:=main}
-: ${GO_VERSION:=1.20}
+: "${CLAIRCORE_BRANCH:=main}"
+: "${GO_VERSION:=1.20}"
 test "${#GO_VERSION}" -gt 4 && GO_VERSION=${GO_VERSION%.*}
 
-cd $(git rev-parse --show-toplevel)
+cd "$(git rev-parse --show-toplevel)"
 echo '#' "$(go version)"
 go mod edit "-go=${GO_VERSION}"\
 	"-replace=github.com/quay/claircore=github.com/quay/claircore@${CLAIRCORE_BRANCH}"
