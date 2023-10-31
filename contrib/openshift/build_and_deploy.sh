135af0e02df0f3a3d2a2d1364a0b929e062732a7
Hank Donnay <hdonnay@redhat.com>
2023-10-31 12:12:13 -0500

    openshift: make build_and_deploy script shellcheck-clean
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- contrib/openshift/build_and_deploy.sh
+++ contrib/openshift/build_and_deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+set -exuo pipefail
 
-set -exv
-REPOSITORY="quay.io/app-sre"
-IMAGE="${REPOSITORY}/clair"
-GIT_HASH=`git rev-parse --short=7 HEAD`
+: "${QUAY_USER:?Missing QUAY_USER variable.}"
+: "${QUAY_TOKEN:?Missing QUAY_TOKEN variable.}"
+: "${REPOSITORY:=quay.io/app-sre}"
+: "${IMAGE:=${REPOSITORY}/clair}"
+GIT_HASH="$(git rev-parse --short=7 HEAD)"
 
-git archive HEAD|
-docker build -t clair-service:latest -
+git archive HEAD |
+	docker build -t clair-service:latest -
 
 skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
     "docker-daemon:clair-service:latest" \
