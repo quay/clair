1fd77f0dcb5cee865e9656365fb9660439a2f28f
Hank Donnay <hdonnay@redhat.com>
2024-02-19 16:16:54 -0600

    httputil: add test for non-OK statuses
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- internal/httputil/responserecorder_test.go
+++ internal/httputil/responserecorder_test.go
@@ -7,21 +7,45 @@ import (
 )
 
 func TestResponseRecorder(t *testing.T) {
-	var status int
-	var length int64
+	t.Run("OK", func(t *testing.T) {
+		var status int
+		var length int64
 
-	rec := httptest.NewRecorder()
-	w := ResponseRecorder(&status, &length, rec)
+		rec := httptest.NewRecorder()
+		w := ResponseRecorder(&status, &length, rec)
 
-	sz := 512
-	if n, err := w.Write(make([]byte, sz)); err != nil || n != sz {
-		t.Errorf("unexpected Write return: (%v, %v)", n, err)
-	}
-	t.Logf("wrote %d bytes, status %q", length, http.StatusText(status))
-	if got, want := status, http.StatusOK; got != want {
-		t.Errorf("bad status; got: %d, want: %d", got, want)
-	}
-	if got, want := length, int64(sz); got != want {
-		t.Errorf("bad length; got: %d, want: %d", got, want)
-	}
+		sz := 512
+		if n, err := w.Write(make([]byte, sz)); err != nil || n != sz {
+			t.Errorf("unexpected Write return: (%v, %v)", n, err)
+		}
+		t.Logf("wrote %d bytes, status %q", length, http.StatusText(status))
+		if got, want := status, http.StatusOK; got != want {
+			t.Errorf("bad status; got: %d, want: %d", got, want)
+		}
+		if got, want := length, int64(sz); got != want {
+			t.Errorf("bad length; got: %d, want: %d", got, want)
+		}
+	})
+
+	t.Run("Error", func(t *testing.T) {
+		var status int
+		var length int64
+		sc := http.StatusInternalServerError
+
+		rec := httptest.NewRecorder()
+		w := ResponseRecorder(&status, &length, rec)
+
+		sz := 512
+		w.WriteHeader(sc)
+		if n, err := w.Write(make([]byte, sz)); err != nil || n != sz {
+			t.Errorf("unexpected Write return: (%v, %v)", n, err)
+		}
+		t.Logf("wrote %d bytes, status %q", length, http.StatusText(status))
+		if got, want := status, sc; got != want {
+			t.Errorf("bad status; got: %d, want: %d", got, want)
+		}
+		if got, want := length, int64(sz); got != want {
+			t.Errorf("bad length; got: %d, want: %d", got, want)
+		}
+	})
 }
