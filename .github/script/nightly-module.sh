c42bee62e2fcad39743be02191055d967d5a15a6
Hank Donnay <hdonnay@redhat.com>
2024-04-18 11:24:59 -0500

    cicd: improve nightly script output
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- .github/script/nightly-module.sh
+++ .github/script/nightly-module.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
 set -e
 : "${CLAIRCORE_BRANCH:=main}"
-
-echo "::group::go version"
 cd "$(git rev-parse --show-toplevel)"
-go version
-echo "::endgroup::"
-
 test -d vendor && rm -rf vendor
 
-echo "::group::Changes"
+echo "::group::Edits"
 go mod edit \
 	"-replace=github.com/quay/claircore=github.com/quay/claircore@${CLAIRCORE_BRANCH}"
 go mod tidy
 go mod download # Shouldn't be needed, but just to be safe...
-git diff
 echo "::endgroup::"
 
 clair_version="$(git describe --tags --always --dirty --match 'v4.*')"
-echo "::notice::Clair version: ${clair_version}"
 echo "clair_version=${clair_version}" >> "$GITHUB_OUTPUT"
+
+cat <<. >>"$GITHUB_STEP_SUMMARY"
+### Changes
+
+- **Go version:** $(go version)
+- **Clair version:** ${clair_version}
+.
+{
+	echo '```patch'
+	git diff
+	echo '```' 
+} >>"$GITHUB_STEP_SUMMARY"
