3b650c56a5ecd4242ecb4926a0b4e62a7a1ef691
Hank Donnay <hdonnay@redhat.com>
2024-04-18 09:30:01 -0500

    cicd: fix nightly build
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- .github/script/nightly-module.sh
+++ .github/script/nightly-module.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 set -e
 : "${CLAIRCORE_BRANCH:=main}"
-: "${GO_VERSION:=1.20}"
-test "${#GO_VERSION}" -gt 4 && GO_VERSION=${GO_VERSION%.*}
 
+echo "::group::go version"
 cd "$(git rev-parse --show-toplevel)"
-echo '#' "$(go version)"
-go mod edit "-go=${GO_VERSION}"\
-	"-replace=github.com/quay/claircore=github.com/quay/claircore@${CLAIRCORE_BRANCH}"
-git diff
+go version
+echo "::endgroup::"
+
 test -d vendor && rm -rf vendor
+
+echo "::group::Changes"
+go mod edit \
+	"-replace=github.com/quay/claircore=github.com/quay/claircore@${CLAIRCORE_BRANCH}"
 go mod tidy
-go mod vendor
-echo "clair_version=$(git describe --tags --always --dirty --match 'v4.*')" >> "$GITHUB_OUTPUT"
+go mod download # Shouldn't be needed, but just to be safe...
+git diff
+echo "::endgroup::"
+
+clair_version="$(git describe --tags --always --dirty --match 'v4.*')"
+echo "::notice::Clair version: ${clair_version}"
+echo "clair_version=${clair_version}" >> "$GITHUB_OUTPUT"
