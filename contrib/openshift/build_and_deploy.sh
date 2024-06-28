8c467459a48386907e5c9a5c7f21e95732e7411d
Hank Donnay <hdonnay@redhat.com>
2024-06-28 11:47:17 -0500

    openshift: add "dry run" flag
    This adds a "dry run" flag (`n`) and a "debug" flag (`x`) instead of
    having shell debugging on unconditionally.
    
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- contrib/openshift/build_and_deploy.sh
+++ contrib/openshift/build_and_deploy.sh
@@ -1,21 +1,35 @@
-#!/bin/bash
-set -exuo pipefail
+#!/usr/bin/bash
+set -euo pipefail
 
-: "${QUAY_USER:?Missing QUAY_USER variable.}"
-: "${QUAY_TOKEN:?Missing QUAY_TOKEN variable.}"
+while getopts nx name; do
+	case "$name" in
+	x) set -x ;;
+	n) dryrun=1 ;;
+	?)
+		printf "Usage: %s: [-nx]\n" "$0"
+		exit 2
+		;;
+	esac
+done
+
 : "${REGISTRY:=quay.io}"
 : "${REPOSITORY:=${REGISTRY}/app-sre}"
 : "${IMAGE:=${REPOSITORY}/clair}"
 GIT_HASH="$(git rev-parse --short=7 HEAD)"
 CONTAINER_ENGINE=$(command -v podman 2>/dev/null || command -v docker 2>/dev/null)
+tags=("${IMAGE}:latest" "${IMAGE}:${GIT_HASH}")
 
 git archive HEAD |
 	${CONTAINER_ENGINE} build -t clair-service:latest -
+for t in "${tags[@]}"; do
+	${CONTAINER_ENGINE} tag clair-service:latest "$t"
+done
 
+[[ -n "$dryrun" ]] && exit 0
+
+: "${QUAY_USER:?Missing QUAY_USER variable.}"
+: "${QUAY_TOKEN:?Missing QUAY_TOKEN variable.}"
 ${CONTAINER_ENGINE} login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" "${REGISTRY}"
-
-${CONTAINER_ENGINE} tag clair-service:latest ${IMAGE}:latest
-${CONTAINER_ENGINE} push ${IMAGE}:latest
-
-${CONTAINER_ENGINE} tag clair-service:latest ${IMAGE}:${GIT_HASH}
-${CONTAINER_ENGINE} push ${IMAGE}:${GIT_HASH}
+for t in "${tags[@]}"; do
+	${CONTAINER_ENGINE} push "$t"
+done
