0b8e3507a0425fde64932627245a66458cb853da
Hank Donnay <hdonnay@redhat.com>
2025-01-10 09:51:54 -0600

    stomp: add deprecation notice
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

---
+++ notifier/stomp/doc.go
@@ -0,0 +1,5 @@
+// Package stomp implements a [Deliverer] over the STOMP protocol.
+//
+// Deprecated: This package will be removed in a future version. Users should
+// write a webhook to STOMP transducer.
+package stomp
