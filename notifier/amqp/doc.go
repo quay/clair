1a9f8769746a484fba6acdd2d4a88da661f1eb28
Hank Donnay <hdonnay@redhat.com>
2025-01-10 09:49:21 -0600

    amqp: add deprecation notice
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

---
+++ notifier/amqp/doc.go
@@ -0,0 +1,5 @@
+// Package amqp implements a [Deliverer] over the AMQP protocol.
+//
+// Deprecated: This package will be removed in a future version. Users should
+// write a webhook to AMQP transducer.
+package amqp
