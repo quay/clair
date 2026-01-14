3e9c72b540283380241cc9eefd13537383f71eab
Hank Donnay <hdonnay@redhat.com>
2026-01-14 10:42:08 -0600

    webhookd: migrate to slog
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- notifier/webhook/cmd/webhookd/main.go
+++ notifier/webhook/cmd/webhookd/main.go
@@ -15,7 +15,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"log"
+	"log/slog"
 	"net"
 	"net/http"
 	"net/http/httputil"
@@ -32,17 +32,35 @@ import (
 	"github.com/quay/clair/v4/notifier"
 )
 
-// This program is unlike the other Clair binaries in that it uses the stdlib
-// "log" package in a format that I like rather than "zlog".
 func main() {
-	debug := flag.Bool("D", false, "print debugging output")
+	var code int
+	ctx, done := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer done()
+	defer func() {
+		done()
+		if code != 0 {
+			os.Exit(code)
+		}
+	}()
+	var level slog.LevelVar
+	flag.BoolFunc("D", "print debugging output", func(arg string) error {
+		l := slog.LevelInfo
+		if ok, _ := strconv.ParseBool(arg); ok {
+			l = slog.LevelDebug
+		}
+		level.Set(l)
+		return nil
+	})
 	addr := flag.String("listen", ":http", "address to listen on")
 	keyEnc := flag.String("key", "", "base64 encoded PSK for signed requests")
 	iss := flag.String("iss", "quay", "issuer for signed requests")
 	flag.Parse()
 
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: &level,
+	})))
+
 	h := &Recv{
-		Debug:  *debug,
 		Client: http.DefaultClient,
 	}
 
@@ -52,25 +70,24 @@ func main() {
 		key := make([]byte, l)
 		n, err := base64.StdEncoding.Decode(key, b)
 		if err != nil {
-			log.Fatal(err)
+			slog.ErrorContext(ctx, "unable to decode key", "reason", err)
+			code = 1
+			return
 		}
 		key = key[:n]
-		if h.Debug {
-			log.Printf("D decoded key: %+#q", key)
-		}
+		slog.DebugContext(ctx, "decoded key", "key", key)
 		sk := jose.SigningKey{
 			Algorithm: jose.HS256,
 			Key:       key,
 		}
 		h.Signer, err = jose.NewSigner(sk, nil)
 		if err != nil {
-			log.Fatal(err)
+			slog.ErrorContext(ctx, "unable to create signer", "reason", err)
+			code = 1
+			return
 		}
 		h.Claim = &jwt.Claims{Issuer: *iss}
 	}
-	ctx := context.Background()
-	ctx, done := signal.NotifyContext(ctx, os.Interrupt)
-	defer done()
 	srv := http.Server{
 		Addr:        *addr,
 		Handler:     h,
@@ -78,15 +95,18 @@ func main() {
 	}
 	go func() {
 		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-			log.Fatal(err)
+			slog.ErrorContext(ctx, "unable to start HTTP server", "reason", err)
+			done()
+			code = 1
+			return
 		}
 	}()
 
-	log.Println(":", "ready")
+	slog.InfoContext(ctx, "ready")
 	defer func() {
-		log.Println(":", "shutting down")
+		slog.InfoContext(ctx, "shutting down")
 		if err := srv.Shutdown(ctx); err != nil && err != context.Canceled {
-			log.Println(err)
+			slog.ErrorContext(ctx, "HTTP server shutdown", "reason", err)
 		}
 	}()
 	<-ctx.Done()
@@ -97,36 +117,34 @@ type Recv struct {
 	Client *http.Client
 	Signer jose.Signer
 	Claim  *jwt.Claims
-	Debug  bool
 }
+
+const contentType = `application/json`
 
 // ServeHTTP implements [http.Handler].
 func (h *Recv) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 	rc := http.NewResponseController(w)
 	defer rc.Flush()
 
-	if h.Debug {
-		b, err := httputil.DumpRequest(r, true)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println("D", "received hook:", strconv.Quote(string(b)))
-	}
+	slog.DebugContext(ctx, "received hook", "request", (*logRequest)(r))
 
 	if r.Method != http.MethodPost {
-		http.Error(w, fmt.Sprintf("bad method: %s", r.Method), http.StatusBadRequest)
-		log.Println("E", "bad method:", r.Method)
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "", http.StatusMethodNotAllowed)
+		slog.WarnContext(ctx, "bad request", "method", r.Method)
 		return
 	}
-	if ct := r.Header.Get(`content-type`); ct != `application/json` {
-		http.Error(w, fmt.Sprintf("bad content-type: %s", ct), http.StatusBadRequest)
-		log.Println("E", "bad content-type:", r.Method)
+	if ct := r.Header.Get(`content-type`); ct != contentType {
+		w.Header().Set("Accept-Post", contentType)
+		http.Error(w, "", http.StatusUnsupportedMediaType)
+		slog.WarnContext(ctx, "bad request", "content-type", ct)
 		return
 	}
 
 	defer func() {
 		if err := r.Body.Close(); err != nil {
-			log.Println("E", "unable to close:", err.Error())
+			slog.WarnContext(ctx, "unable to close request body", "reason", err)
 			panic(http.ErrAbortHandler)
 		}
 	}()
@@ -134,17 +152,17 @@ func (h *Recv) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	dec := json.NewDecoder(r.Body)
 	if err := dec.Decode(&payload); err != nil {
 		http.Error(w, fmt.Sprintf("bad content: %v", err), http.StatusBadRequest)
-		log.Println("E", "bad content:", err.Error())
+		slog.WarnContext(ctx, "bad payload", "reason", err)
 		return
 	}
 	whid := path.Base(payload.Callback.Path)
 
 	var resp response
 	for next := new(uuid.UUID); next != nil; next = resp.Page.Next {
-		req, err := http.NewRequestWithContext(r.Context(), http.MethodGet, payload.Callback.String(), nil)
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, payload.Callback.String(), nil)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("unable to create request: %v", err), http.StatusInternalServerError)
-			log.Println("E", "unable to create request:", err.Error())
+			slog.WarnContext(ctx, "unable to create request", "reason", err)
 			return
 		}
 		if pg := resp.Page.Next; pg != nil {
@@ -154,86 +172,69 @@ func (h *Recv) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 		if err := h.sign(req); err != nil {
 			http.Error(w, fmt.Sprintf("unable to sign request: %v", err), http.StatusInternalServerError)
-			log.Println("E", "unable to sign request:", err.Error())
+			slog.WarnContext(ctx, "unable to sign request", "reason", err)
 			return
 		}
 
-		if h.Debug {
-			b, err := httputil.DumpRequest(req, true)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Println("D", "making request:", strconv.Quote(string(b)))
-		}
+		slog.DebugContext(ctx, "making request", "request", (*logRequest)(req))
 
 		res, err := http.DefaultClient.Do(req)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("error making request: %v", err), http.StatusInternalServerError)
-			log.Println("E", "unable to make request:", err.Error())
+			slog.WarnContext(ctx, "unable to make request", "reason", err)
 			return
 		}
 		defer res.Body.Close()
 
-		if h.Debug {
-			b, err := httputil.DumpResponse(res, true)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Println("D", "got response:", strconv.Quote(string(b)))
-		}
-
+		slog.DebugContext(ctx, "got response", "response", (*logResponse)(res))
 		if res.StatusCode != http.StatusOK {
 			http.Error(w, fmt.Sprintf("bad response from upstream: %q", res.Status), http.StatusInternalServerError)
-			log.Println("E", "bad status:", res.Status)
+			slog.WarnContext(ctx, "bad status", "status", res.Status)
 			return
 		}
 
 		if err := json.NewDecoder(res.Body).Decode(&resp); err != nil {
 			http.Error(w, fmt.Sprintf("bad content from upstream: %v", err), http.StatusTeapot)
-			log.Println("E", "bad content:", err)
+			slog.WarnContext(ctx, "bad content", "reason", err)
 			return
 		}
 
 		for _, n := range resp.Notifications {
-			log.Println(":", whid, n.ID, n.Manifest, n.Reason, n.Vulnerability.Name)
+			slog.InfoContext(ctx, "notification", "id", whid, slog.Group("notification",
+				"id", n.ID,
+				"manifest", n.Manifest,
+				"reason", n.Reason,
+				"name", n.Vulnerability.Name,
+			))
 		}
 	}
-	req, err := http.NewRequestWithContext(r.Context(), http.MethodDelete, payload.Callback.String(), nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodDelete, payload.Callback.String(), nil)
 	if err != nil {
-		log.Fatal(err)
+		http.Error(w, fmt.Sprintf("unable to create request: %v", err), http.StatusInternalServerError)
+		slog.WarnContext(ctx, "unable to create request", "reason", err)
+		return
 	}
 	if err := h.sign(req); err != nil {
 		http.Error(w, fmt.Sprintf("unable to sign request: %v", err), http.StatusInternalServerError)
-		log.Println("E", "unable to sign request:", err.Error())
+		slog.WarnContext(ctx, "unable to sign request", "reason", err)
 		return
 	}
-	if h.Debug {
-		b, err := httputil.DumpRequest(req, true)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println("D", "making request:", strconv.Quote(string(b)))
-	}
+	slog.DebugContext(ctx, "making request", "request", (*logRequest)(req))
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("error making request: %v", err), http.StatusInternalServerError)
+		slog.WarnContext(ctx, "unable to make request", "reason", err)
 		return
 	}
 	defer res.Body.Close()
-	if h.Debug {
-		b, err := httputil.DumpResponse(res, true)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println("D", "got response:", strconv.Quote(string(b)))
-	}
+	slog.DebugContext(ctx, "got response", "response", (*logResponse)(res))
 	if res.StatusCode != http.StatusOK {
 		http.Error(w, fmt.Sprintf("bad response from upstream: %q", res.Status), http.StatusInternalServerError)
-		log.Println("E", "bad status:", res.Status)
+		slog.WarnContext(ctx, "bad status", "status", res.Status)
 		return
 	}
-	log.Println(":", "deleted:", whid)
+	slog.InfoContext(ctx, "deleted", "id", whid)
 }
 
 // Response is a page of notifications.
@@ -258,4 +259,33 @@ func (h *Recv) sign(req *http.Request) error {
 	}
 	req.Header.Set("authorization", "Bearer "+tok)
 	return nil
+}
+
+var (
+	_ slog.LogValuer = (*logRequest)(nil)
+	_ slog.LogValuer = (*logResponse)(nil)
+)
+
+type logRequest http.Request
+
+// LogValue implements [slog.LogValuer].
+func (l *logRequest) LogValue() slog.Value {
+	req := (*http.Request)(l)
+	b, err := httputil.DumpRequest(req, true)
+	if err != nil {
+		return slog.GroupValue(slog.String("error", err.Error()))
+	}
+	return slog.StringValue(string(b))
+}
+
+type logResponse http.Response
+
+// LogValue implements [slog.LogValuer].
+func (l *logResponse) LogValue() slog.Value {
+	res := (*http.Response)(l)
+	b, err := httputil.DumpResponse(res, true)
+	if err != nil {
+		return slog.GroupValue(slog.String("error", err.Error()))
+	}
+	return slog.StringValue(string(b))
 }
