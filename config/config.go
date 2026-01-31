42f6a9c4419c1139f5be7241b26e846016c83bf4
Hank Donnay <hdonnay@redhat.com>
2026-01-30 19:01:00 -0600

    config: documentation updates
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- config/config.go
+++ config/config.go
@@ -12,23 +12,25 @@ type Config struct {
 	// TLS configures HTTPS support.
 	//
 	// Note that any non-trivial deployment means the certificate provided here
-	// will need to be for the name the load balancer uses to connect to a given
-	// Clair instance.
+	// will need to be for the name used by the load balancer for a given Clair
+	// instance.
 	//
-	// This is not used for outgoing requests; setting the SSL_CERT_DIR
-	// environment variable is the recommended way to do that. The release
-	// container has `/var/run/certs` added to the list already.
+	// Deprecated: Use the [API] configuration hierarchy.
 	TLS *TLS `yaml:"tls,omitempty" json:"tls,omitempty"`
 	// Sets which mode the clair instance will run.
 	Mode Mode `yaml:"-" json:"-"`
 	// A string in <host>:<port> format where <host> can be an empty string.
 	//
-	// exposes Clair node's functionality to the network.
-	// see /openapi/v1 for api spec.
+	// Exposes Clair node's functionality to the network.
+	// See /openapi/v1 for the API spec.
+	//
+	// Deprecated: Use the [API] configuration hierarchy.
 	HTTPListenAddr string `yaml:"http_listen_addr" json:"http_listen_addr"`
 	// A string in <host>:<port> format where <host> can be an empty string.
 	//
-	// exposes Clair's metrics and health endpoints.
+	// Exposes Clair's metrics and health endpoints.
+	//
+	// Deprecated: Use the [Introspection] configuration hierarchy.
 	IntrospectionAddr string `yaml:"introspection_addr" json:"introspection_addr"`
 	// Set the logging level.
 	LogLevel      LogLevel      `yaml:"log_level" json:"log_level"`
